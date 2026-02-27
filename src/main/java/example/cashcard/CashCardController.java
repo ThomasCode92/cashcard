eccf9d4b425918ce7fae0cddff29c81f28c3d228
Thomas <thomascode92@hotmail.com>
2026-02-27 21:00:09 +0100

    Repositories & Spring Data

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+  private final CashCardRepository cashCardRepository;
+
+  private CashCardController(CashCardRepository cashCardRepository) {
+    this.cashCardRepository = cashCardRepository;
+  }
+
   @GetMapping("/{requestId}")
   private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
-    if (requestId.equals(99L)) {
-      CashCard cashCard = new CashCard(99L, 123.45);
-      return ResponseEntity.ok(cashCard);
+    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestId);
+    if (cashCardOptional.isPresent()) {
+      return ResponseEntity.ok(cashCardOptional.get());
     } else {
       return ResponseEntity.notFound().build();
     }
