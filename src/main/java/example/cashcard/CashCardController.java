d4d61220ee7f290ef2c97127651ab7910aa157c2
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2026-03-04 21:46:55 +0100

    Creating RESTful Endpoints (#1)

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,30 @@
+package example.cashcard;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+  private final CashCardRepository cashCardRepository;
+
+  private CashCardController(CashCardRepository cashCardRepository) {
+    this.cashCardRepository = cashCardRepository;
+  }
+
+  @GetMapping("/{requestId}")
+  private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
+    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestId);
+    if (cashCardOptional.isPresent()) {
+      return ResponseEntity.ok(cashCardOptional.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+}
